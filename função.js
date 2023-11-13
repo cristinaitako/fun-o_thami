4f7802d873454cbd53144c2b0a9770d494951e2a
cristinaitako <125576531+cristinaitako@users.noreply.github.com>
2023-11-13 12:27:41 -0300

    Add files via upload

---
+++ função.js
@@ -0,0 +1,11 @@
+function LeiaSeuNome(autor){
+    if (autor == "Thamires"){
+        leia("Eu Amo Comida");
+    }
+}
+const autor["Thamires"];
+let contador = 0;
+while(contador < autor.length){
+    LeiaSeuNome(autor[contador]);
+    contador = contador +1 
+}
